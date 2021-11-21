d3b9ffb73fca911e72848cc98959630063691602
Adam Greloch <zplhatesbananas@gmail.com>
2021-11-21 22:31:44 +0100

    [3-iset] przechodzi testy

--- 3-iset/iSet.ml
+++ 3-iset/iSet.ml
@@ -20,8 +20,12 @@ let elenum = function
     | Empty -> 0
 
 let make l ((a,b) as v) r =
-    (* TODO: obsługa nieskończoności *)
-    let e = (-a) ++ b ++ elenum l ++ elenum r ++ 1 in
+    let e =
+    if a = min_int then
+        -(a+1) ++ b ++ 2 ++ elenum l ++ elenum r
+    else
+        (-a) ++ b ++ 1 ++ elenum l ++ elenum r
+    in
     Node (l, v, r, max (height l) (height r) + 1, e)
 
 let bal l v r =
@@ -58,6 +62,16 @@ let rec remove_min_elt = function
     | Node (Empty, _, r, _, _) -> r
     | Node (l, v, r, _, _) -> bal (remove_min_elt l) v r
     | Empty -> invalid_arg "ISet.remove_min_elt"
+
+let rec max_elt = function
+    | Node (_, i, Empty, _, _) -> i
+    | Node (_, _, r, _, _) -> max_elt r
+    | Empty -> raise Not_found
+
+let rec remove_max_elt = function
+    | Node (l, _, Empty, _, _) -> l
+    | Node (l, v, r, _, _) -> bal l v (remove_max_elt r)
+    | Empty -> invalid_arg "ISet.remove_max_elt"
 
 let merge t1 t2 =
     match (t1, t2) with
@@ -100,26 +114,29 @@ let rec split a = function
         if a < b then
             let (ll, pres, rl) = split a l in (ll, pres, join rl v r)
         else if b <= a && a <= c then
-            (add_one (b,a-1) l, true, add_one (a+1,c) r)
+            if a = min_int then
+                (add_one (b,a) l, false, add_one (a++1,c) r)
+            else
+                (add_one (b,a++(-1)) l, true, add_one (a++1,c) r)
         else
             let (lr, pres, rr) = split a r in (join l v lr, pres, rr)
 
 let rec add ((a,b) as x) = function
-    (* TODO: obsługa nieskończoności *)
     | Empty -> make Empty x Empty
-    | Node (l, (c,d), r, _, _) as s ->
+    | Node (_, (c,d), _, _, _) as s ->
         if c <= a && b <= d then s else
-        let (a,b) = if b = c || b ++ 1 = c then (a,d)
-            else if a = d || a ++ (-1) = d then (c,b)
-            else (a,b)
+        let (al, _, _) = split a s
+        and (_, _, br) = split b s
         in
-        let (al, apres, _) = split (a-1) s
-        and (_, bpres, br) = split (b+1) s
+        let (lc, ld) = if al = Empty then (a,a) else max_elt al
+        and (rc, rd) = if br = Empty then (b,b) else min_elt br
         in
-        let a = if apres then a-1 else a
-        and b = if bpres then b+1 else b
+        let (al,a) = if al <> Empty && (ld = a || ld = a ++ (-1)) then
+            (remove_max_elt al, lc) else (al, a)
+        and (br,b) = if br <> Empty && (rc = b || rc = b ++ 1) then
+            (remove_min_elt br, rd) else (br, b)
         in
-        join al (a,b) br
+            join al (a,b) br
 
 let remove (a,b) s = 
     let (al, _, _) = split a s
@@ -163,3 +180,37 @@ let below n s =
     in
     elenum l ++ (if pres then 1 else 0)
 
+let a = add (0, 5) empty;;
+let a = add (7, 8) a;;
+let a = add (-3, -3) a;;
+let a = add (10, 13) a;;
+assert(elements a = [(-3, -3); (0, 5); (7, 8); (10, 13)]);;
+assert(below 8 a = 9);;
+let b = add (6, 6) a;;
+let b = remove (6, 6) b;;
+let b = add (-100, -5) b;;
+let b = add (-4, 6) b;;
+assert(elements b = [(-100, 8); (10, 13)]);;
+assert(below 10 b = 110);;
+let c = remove (2, 10) a;;
+assert(elements c = [(-3, -3); (0, 1); (11, 13)]);;
+assert(below 12 c = 5);;
+
+let zle = ref 0
+let test (id:int) (result:bool) (expected:bool) : unit =
+    if result <> expected then begin
+        Printf.printf "Zly wynik testu %d!\n" id;
+        incr zle
+    end;;
+
+let s = add (3, 4) (add (8, 10) (add (15, 20) empty));;
+test 51 (below 2 s = 0) true;;
+test 52 (below 3 s = 1) true;;
+test 53 (below 10 s = 5) true;;
+test 54 (below 15 s = 6) true;;
+test 55 (below 100 s = 11) true;;
+let s = add (1, max_int) (add (-1, 0) empty);;
+test 56 (below max_int s = max_int) true;;
+let s = add (-min_int, max_int) empty;;
+test 57 (below max_int s = max_int) true;;
+test 58 (below min_int s = 1) true;;
